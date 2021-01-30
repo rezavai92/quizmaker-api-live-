19a53f4b79bb9f96d882e8137febdc6869ff4192
rezavai92 <nijhumreza52@gmail.com>
2021-01-31 00:43:09 +0600

    preparing to deploy in heroku

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bp = require('body-parser')
 const quiz =require('./Routes/quiz')
 const user = require('./Routes/user')
+const path = require('path')
 const auth = require('./Routes/auth')
 const app = express()
 
@@ -12,7 +13,20 @@ app.use(bp.urlencoded({extended:true}))
 app.use('/quiz',quiz)
 app.use('/user',user)
 app.use('/auth',auth)
-const port = 5000||process.env.PORT
+const port = 5000||process.env.PORT;
+
+
+
+if(process.env.NODE_ENV==="production"){
+
+
+    app.use(express.static('client/build'));
+
+    app.get("*",(req,res)=>{
+
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    } )
+}
 app.listen(port,()=>{
 
     console.log("app is listening to ",port)
